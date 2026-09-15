8b803e7c78f12aae4f0d0f9157c10d246ecdf25c
pratikshevkar <contactpratikshevkar@gmail.com>
2026-09-15 15:03:03 +0530

    eod 15.9.2026

---
+++ loop.py
@@ -0,0 +1,38 @@
+
+# while True:
+#     str = input("enter string")
+#     print(str)
+# i=2
+# cnt=0
+# while (i<=10):
+#     cnt = cnt +1
+#     i = i+2
+# print(cnt)
+
+# i =0
+# while i<10:
+#     if i==5:
+#         continue
+#     print(i)
+#     i += 1
+
+# num = 2
+# i = 1
+# while(i<=10):
+#     print(f"{num}* {i} = {num*i}")
+#     i= i+1
+i = 1
+i = 2
+# while(i<=100):
+#     for(j = 2;j<i/2;j++) {
+#         if(i%j==0):
+#         print(i)
+#     }
+#     i = i+1
+
+L = [4,6,3,6,4]
+sum = 0
+for n in L:
+    sum = n + sum
+print(sum)
+print(sum/len(L))
